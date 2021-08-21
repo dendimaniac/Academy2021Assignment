22c2d343852b20189677bc3b63125bae0b9f0550
Quan Dao <quan@3dbear.fi>
2021-08-21 19:16:23 +0300

    Added color map to allow easy color switch

---
+++ Assets/Scripts/ColorMap.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace ColorSwitch
+{
+    [Serializable]
+    public class ColorMap
+    {
+        public GameColor gameColor;
+        public Color color;
+    }
+}
