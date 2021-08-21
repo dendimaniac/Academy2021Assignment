61f0177dc676f6d3e352241826c93442f991edb8
Quan Dao <quan@3dbear.fi>
2021-08-21 19:35:36 +0300

    Added StarCountUI.cs to show star count during gameplay

---
+++ Assets/Scripts/StarCountUI.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+namespace ColorSwitch
+{
+    public class StarCountUI : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text starCountText;
+        [SerializeField] private CanvasGroup starCountCanvasGroup;
+        [Range(0, 1f)] [SerializeField] private float fadeAmount;
+        [SerializeField] private StarPickedUpEventChannel starPickedUpEventChannel;
+        [SerializeField] private PlayerDiedEventChannel playerDiedEventChannel;
+
+        private int _currentStarCount;
+
+        private void Awake()
+        {
+            starPickedUpEventChannel.StarPickedUp += OnStarPickedUp;
+            playerDiedEventChannel.PlayerDied += OnPlayerDied;
+        }
+
+        private void OnStarPickedUp(Vector3 _)
+        {
+            ++_currentStarCount;
+            starCountText.text = $"{_currentStarCount}";
+        }
+
+        private void OnPlayerDied(PlayerController obj)
+        {
+            starCountCanvasGroup.alpha = fadeAmount;
+        }
+
+        private void OnDestroy()
+        {
+            starPickedUpEventChannel.StarPickedUp -= OnStarPickedUp;
+            playerDiedEventChannel.PlayerDied -= OnPlayerDied;
+        }
+    }
+}
