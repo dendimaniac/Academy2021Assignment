221eb820a61d21f5fb70540f20746be3f1f1633b
Quan Dao <quan@3dbear.fi>
2021-08-21 19:34:35 +0300

    Added PlayerDeathEffect.cs to instantiate player death particle systems at any player position

---
+++ Assets/Scripts/PlayerDeathEffect.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace ColorSwitch
+{
+    public class PlayerDeathEffect : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem playerDeath;
+        [SerializeField] private PlayerDiedEventChannel playerDiedEventChannel;
+
+        private void Awake()
+        {
+            playerDiedEventChannel.PlayerDied += OnPlayerDied;
+        }
+
+        private void OnPlayerDied(PlayerController playerController)
+        {
+            Instantiate(playerDeath, playerController.transform.position, Quaternion.identity);
+        }
+
+        private void OnDestroy()
+        {
+            playerDiedEventChannel.PlayerDied -= OnPlayerDied;
+        }
+    }
+}
