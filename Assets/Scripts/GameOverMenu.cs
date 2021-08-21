bb55e9f0729e1867067540954b1abc2fd289c684
Quan Dao <quan@3dbear.fi>
2021-08-21 19:33:46 +0300

    Added GameOverMenu.cs to show high score and instruction when player died

---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+
+namespace ColorSwitch
+{
+    public class GameOverMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject gameOverMenu;
+        [SerializeField] private TMP_Text highScoreText;
+
+        [SerializeField] private StarPickedUpEventChannel starPickedUpEventChannel;
+        [SerializeField] private PlayerDiedEventChannel playerDiedEventChannel;
+
+        private int _currentScore;
+
+        private void Awake()
+        {
+            playerDiedEventChannel.PlayerDied += OnPlayerDied;
+            starPickedUpEventChannel.StarPickedUp += OnStarPickedUp;
+        }
+
+        private void OnPlayerDied(PlayerController obj)
+        {
+            gameOverMenu.SetActive(true);
+            HighScoreManager.SaveScore(_currentScore);
+            highScoreText.text = $"{HighScoreManager.GetHighScore()}";
+        }
+        
+        private void OnStarPickedUp(Vector3 _)
+        {
+            ++_currentScore;
+        }
+
+        private void OnDestroy()
+        {
+            playerDiedEventChannel.PlayerDied -= OnPlayerDied;
+            starPickedUpEventChannel.StarPickedUp -= OnStarPickedUp;
+        }
+    }
+}
