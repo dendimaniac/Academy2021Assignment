fddbc66fd8ae7ce2d5ef21e8e478f1b700c50630
Quan Dao <quan@3dbear.fi>
2021-08-22 23:53:26 +0300

    Refactored AudioManager.cs from being a singleton class

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,21 +7,8 @@ namespace ColorSwitch
         [SerializeField] private AudioSource soundEffectAudioSource;
         [SerializeField] private SoundEffectEventChannelSO soundEffectEventChannel;
 
-        private static AudioManager _instance;
-
         private void Awake()
         {
-            if (_instance == null)
-            {
-                _instance = this;
-                DontDestroyOnLoad(this);
-            }
-            else
-            {
-                Destroy(gameObject);
-                return;
-            }
-            
             soundEffectEventChannel.RequestedSoundEffectClip += OnRequestedSoundEffectClip;
         }
 
