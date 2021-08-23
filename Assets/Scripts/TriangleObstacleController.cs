b54712fb1cff0ddb86d823ae43e79b512e124570
Quan Dao <quan@3dbear.fi>
2021-08-23 21:35:59 +0300

    Added triangle obstacle type

---
+++ Assets/Scripts/TriangleObstacleController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace ColorSwitch
+{
+    public class TriangleObstacleController : ObstacleController
+    {
+        public override void Init(GameColor expectedGameColor)
+        {
+            base.Init(expectedGameColor);
+            var randomObstacleSectionIndex = Random.Range(0, obstacleColorSectionList.Count);
+            obstacleColorSectionList[randomObstacleSectionIndex].OverrideDefaultGameColor(expectedGameColor);
+        }
+    }
+}
